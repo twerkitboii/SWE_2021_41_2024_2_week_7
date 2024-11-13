bf41d56379abced1ff22e3bff038d0850727b373
twerkitboii <kimsiyoon0407@gmail.com>
2024-11-13 18:32:29 +0900

    test for main

--- main.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-def twoSum(nums: List[int], target: int)-> List[int]:
-    for i in range (len(nums)):
-        for j in range(i+1,len{nums}):
-            if(List[i]+List[j]==target):
-                a=i
-                b=j
-                return [a,b]
